21cc3af7c1daa4659f25a79739c397058710b8c7
ScareCrow <12scarecrow21@gmail.com>
2025-07-04 11:43:24 +0300

    Обучена ML.Model по файлу anime.csv. На форму добавлены 6 textbox и 1 buttom. Ошибки : Постоянно Выдаёт рейтинг 5.25.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,17 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button1 = new Button();
+            txtName = new TextBox();
+            txtGenre = new TextBox();
+            txtType = new TextBox();
+            txtEpisodes = new TextBox();
+            txtMembers = new TextBox();
+            lblResult = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(362, 117);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += btnPredict_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(29, 2);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 1;
+            // 
+            // txtGenre
+            // 
+            txtGenre.Location = new Point(29, 31);
+            txtGenre.Name = "txtGenre";
+            txtGenre.Size = new Size(100, 23);
+            txtGenre.TabIndex = 2;
+            // 
+            // txtType
+            // 
+            txtType.Location = new Point(29, 60);
+            txtType.Name = "txtType";
+            txtType.Size = new Size(100, 23);
+            txtType.TabIndex = 3;
+            // 
+            // txtEpisodes
+            // 
+            txtEpisodes.Location = new Point(29, 89);
+            txtEpisodes.Name = "txtEpisodes";
+            txtEpisodes.Size = new Size(100, 23);
+            txtEpisodes.TabIndex = 4;
+            // 
+            // txtMembers
+            // 
+            txtMembers.Location = new Point(29, 118);
+            txtMembers.Name = "txtMembers";
+            txtMembers.Size = new Size(100, 23);
+            txtMembers.TabIndex = 5;
+            // 
+            // lblResult
+            // 
+            lblResult.Location = new Point(240, 2);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(197, 23);
+            lblResult.TabIndex = 6;
+            lblResult.TextChanged += textBox6_TextChanged;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(lblResult);
+            Controls.Add(txtMembers);
+            Controls.Add(txtEpisodes);
+            Controls.Add(txtType);
+            Controls.Add(txtGenre);
+            Controls.Add(txtName);
+            Controls.Add(button1);
             Name = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox txtName;
+        private TextBox txtGenre;
+        private TextBox txtType;
+        private TextBox txtEpisodes;
+        private TextBox txtMembers;
+        private TextBox lblResult;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,3 +1,6 @@
+using static The_Rating_Predictor.MLModel1;
+using System.Xml.Linq;
+
 namespace The_Rating_Predictor
 {
     public partial class Form1 : Form
@@ -6,6 +9,45 @@ namespace The_Rating_Predictor
         {
             InitializeComponent();
         }
+        private void btnPredict_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Проверка: все поля должны быть заполнены
+                if (string.IsNullOrWhiteSpace(txtName.Text) ||
+                    string.IsNullOrWhiteSpace(txtGenre.Text) ||
+                    string.IsNullOrWhiteSpace(txtType.Text) ||
+                    string.IsNullOrWhiteSpace(txtEpisodes.Text) ||
+                    string.IsNullOrWhiteSpace(txtMembers.Text))
+                {
+                    MessageBox.Show("Пожалуйста, заполните все поля.");
+                    return;
+                }
 
+                // Создание входной структуры
+                var input = new ModelInput()
+                {
+                    Anime_id = 9999F, // Просто заглушка
+                    Name = txtName.Text,
+                    Genre = txtGenre.Text,
+                    Type = txtType.Text,
+                    Episodes = txtEpisodes.Text,
+                    Members = txtMembers.Text,
+                };
+
+                // Получение предсказания
+                var prediction = MLModel1.Predict(input);
+                lblResult.Text = $"Прогнозируемый рейтинг: {prediction.Score:F2}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
